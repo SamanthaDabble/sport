7a8f9cd3620111b842a5fc0bf9d5a63078062f10
Brakke <bradanp@dunwoody.edu>
2018-02-22 12:15:53 -0600

    More Json work, just finished total_kick_return_yards

--- newJsonWork.js
+++ newJsonWork.js
@@ -7,14 +7,14 @@ const jsonFile = {
 		total_penalty_yards: 732,
 		rushing: {
 			total_rushing_yards: 1662,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 15
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3925,
+			total_passing_touchdowns: 15
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 673,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -36,14 +36,14 @@ const jsonFile = {
 		total_penalty_yards: 872,
 		rushing: {
 			total_rushing_yards: 1788,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 13
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 2811,
+			total_passing_touchdowns: 13
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 640,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -65,14 +65,14 @@ const jsonFile = {
 		total_penalty_yards: 949,
 		rushing: {
 			total_rushing_yards: 1366,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 6
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3122,
+			total_passing_touchdowns: 25
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 619,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -94,14 +94,14 @@ const jsonFile = {
 		total_penalty_yards: 682,
 		rushing: {
 			total_rushing_yards: 2017,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 12
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 2825,
+			total_passing_touchdowns: 16
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 760,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -123,14 +123,14 @@ const jsonFile = {
 		total_penalty_yards: 835,
 		rushing: {
 			total_rushing_yards: 1852,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 8
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3333,
+			total_passing_touchdowns: 19
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 622,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -152,14 +152,14 @@ const jsonFile = {
 		total_penalty_yards: 922,
 		rushing: {
 			total_rushing_yards: 1714,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 11
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3228,
+			total_passing_touchdowns: 15
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 1001,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -181,14 +181,14 @@ const jsonFile = {
 		total_penalty_yards: 880,
 		rushing: {
 			total_rushing_yards: 1450,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 8
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 4366,
+			total_passing_touchdowns: 26
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 592,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -210,14 +210,14 @@ const jsonFile = {
 		total_penalty_yards: 936,
 		rushing: {
 			total_rushing_yards: 1386,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 6
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3640,
+			total_passing_touchdowns: 21
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 551,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -239,14 +239,14 @@ const jsonFile = {
 		total_penalty_yards: 789,
 		rushing: {
 			total_rushing_yards: 1595,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 10
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 4431,
+			total_passing_touchdowns: 28
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 761,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -268,14 +268,14 @@ const jsonFile = {
 		total_penalty_yards: 943,
 		rushing: {
 			total_rushing_yards: 1903,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 12
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 4104,
+			total_passing_touchdowns: 26
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 969,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -297,14 +297,14 @@ const jsonFile = {
 		total_penalty_yards: 1050,
 		rushing: {
 			total_rushing_yards: 1661,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 11
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 2892,
+			total_passing_touchdowns: 13
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 558,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -326,14 +326,14 @@ const jsonFile = {
 		total_penalty_yards: 1044,
 		rushing: {
 			total_rushing_yards: 2170,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 18
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3141,
+			total_passing_touchdowns: 22
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 1051,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -355,14 +355,14 @@ const jsonFile = {
 		total_penalty_yards: 1035,
 		rushing: {
 			total_rushing_yards: 1388,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 4
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3535,
+			total_passing_touchdowns: 24
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 683,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -384,14 +384,14 @@ const jsonFile = {
 		total_penalty_yards: 895,
 		rushing: {
 			total_rushing_yards: 2115,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 9
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3737,
+			total_passing_touchdowns: 38
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 609,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -413,14 +413,14 @@ const jsonFile = {
 		total_penalty_yards: 939,
 		rushing: {
 			total_rushing_yards: 1847,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 12
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3990,
+			total_passing_touchdowns: 21
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 620,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -442,14 +442,14 @@ const jsonFile = {
 		total_penalty_yards: 1342,
 		rushing: {
 			total_rushing_yards: 1549,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 6
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3479,
+			total_passing_touchdowns: 20
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 1004,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -471,14 +471,14 @@ const jsonFile = {
 		total_penalty_yards: 785,
 		rushing: {
 			total_rushing_yards: 2262,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 18
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3593,
+			total_passing_touchdowns: 21
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 618,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -500,14 +500,14 @@ const jsonFile = {
 		total_penalty_yards: 927,
 		rushing: {
 			total_rushing_yards: 1702,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 13
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3182,
+			total_passing_touchdowns: 19
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 474,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -529,14 +529,14 @@ const jsonFile = {
 		total_penalty_yards: 749,
 		rushing: {
 			total_rushing_yards: 1221,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 10
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 4183,
+			total_passing_touchdowns: 29
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 936,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -558,14 +558,14 @@ const jsonFile = {
 		total_penalty_yards: 885,
 		rushing: {
 			total_rushing_yards: 1724,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 13
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3167,
+			total_passing_touchdowns: 25
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 768,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -587,14 +587,14 @@ const jsonFile = {
 		total_penalty_yards: 962,
 		rushing: {
 			total_rushing_yards: 2102,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 15
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3077,
+			total_passing_touchdowns: 22
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 825,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -616,14 +616,14 @@ const jsonFile = {
 		total_penalty_yards: 988,
 		rushing: {
 			total_rushing_yards: 1889,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 16
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 4418,
+			total_passing_touchdowns: 32
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 570,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -645,14 +645,14 @@ const jsonFile = {
 		total_penalty_yards: 1009,
 		rushing: {
 			total_rushing_yards: 1554,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 13
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3631,
+			total_passing_touchdowns: 23
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 738,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -674,14 +674,14 @@ const jsonFile = {
 		total_penalty_yards: 924,
 		rushing: {
 			total_rushing_yards: 1953,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 17
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3831,
+			total_passing_touchdowns: 28
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 790,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -703,14 +703,14 @@ const jsonFile = {
 		total_penalty_yards: 962,
 		rushing: {
 			total_rushing_yards: 1856,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 14
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3030,
+			total_passing_touchdowns: 20
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 354,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -732,14 +732,14 @@ const jsonFile = {
 		total_penalty_yards: 968,
 		rushing: {
 			total_rushing_yards: 1448,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 10
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3751,
+			total_passing_touchdowns: 27
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 583,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -761,14 +761,14 @@ const jsonFile = {
 		total_penalty_yards: 779,
 		rushing: {
 			total_rushing_yards: 2070,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 23
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 4189,
+			total_passing_touchdowns: 23
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 590,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -790,14 +790,14 @@ const jsonFile = {
 		total_penalty_yards: 1154,
 		rushing: {
 			total_rushing_yards: 1629,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 4
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3657,
+			total_passing_touchdowns: 34
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 834,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -819,14 +819,14 @@ const jsonFile = {
 		total_penalty_yards: 795,
 		rushing: {
 			total_rushing_yards: 1667,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 12
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 4380,
+			total_passing_touchdowns: 29
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 432,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -848,14 +848,14 @@ const jsonFile = {
 		total_penalty_yards: 918,
 		rushing: {
 			total_rushing_yards: 1842,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 8
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3278,
+			total_passing_touchdowns: 28
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 871,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -877,14 +877,14 @@ const jsonFile = {
 		total_penalty_yards: 1027,
 		rushing: {
 			total_rushing_yards: 1833,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 18
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3191,
+			total_passing_touchdowns: 14
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 670,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
@@ -906,14 +906,14 @@ const jsonFile = {
 		total_penalty_yards: 896,
 		rushing: {
 			total_rushing_yards: 1957,
-			total_rushing_touchdowns: 53
+			total_rushing_touchdowns: 15
 		},
 		passing: {
-			total_passing_yards: 2233,
-			total_passing_touchdowns: 1234
+			total_passing_yards: 3753,
+			total_passing_touchdowns: 25
 		},
 		special_teams: {
-			total_kick_return_yards: 42,
+			total_kick_return_yards: 630,
 			total_punt_return_yards: 23
 		},
 		turnovers: {
