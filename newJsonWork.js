756b21744b39b003dddbebb58d48dbcab3f3f6cd
Brakke <bradanp@dunwoody.edu>
2018-02-22 10:35:06 -0600

    Began creating final JSON object

---
+++ newJsonWork.js
@@ -0,0 +1,930 @@
+const jsonFile = {
+    '49ers':{
+		logo_url: '',
+		wins: 6,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Bears':{
+		logo_url: '',
+		wins: 5,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Bengals':{
+		logo_url: '',
+		wins: 7,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Bills':{
+		logo_url: '',
+		wins: 9,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Broncos':{
+		logo_url: '',
+		wins: 5,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Browns':{
+		logo_url: '',
+		wins: 0,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Buccaneers':{
+		logo_url: '',
+		wins: 5,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Cardinals':{
+		logo_url: '',
+		wins: 8,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Chargers':{
+		logo_url: '',
+		wins: 9,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Chiefs':{
+		logo_url: '',
+		wins: 10,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Colts':{
+		logo_url: '',
+		wins: 4,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Cowboys':{
+		logo_url: '',
+		wins: 9,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Dolphins':{
+		logo_url: '',
+		wins: 6,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Eagles':{
+		logo_url: '',
+		wins: 13,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Falcons':{
+		logo_url: '',
+		wins: 10,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Giants':{
+		logo_url: '',
+		wins: 3,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Jaguars':{
+		logo_url: '',
+		wins:10,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Jets':{
+		logo_url: '',
+		wins:5,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Lions':{
+		logo_url: '',
+		wins: 9,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Packers':{
+		logo_url: '',
+		wins: 7,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Panthers':{
+		logo_url: '',
+		wins: 11,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Patriots':{
+		logo_url: '',
+		wins: 13,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Raiders':{
+		logo_url: '',
+		wins: 6,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Rams':{
+		logo_url: '',
+		wins: 11,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Ravens':{
+		logo_url: '',
+		wins: 9,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Redskins':{
+		logo_url: '',
+		wins: 7,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Saints':{
+		logo_url: '',
+		wins: 11,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Seahawks':{
+		logo_url: '',
+		wins: 9,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Steelers':{
+		logo_url: '',
+		wins:13,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Texans':{
+		logo_url: '',
+		wins:4,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Titans':{
+		logo_url: '',
+		wins:9,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+    'Vikings':{
+		logo_url: '',
+		wins:13,
+		losses: 33,
+		total_yards: 45,
+		total_penalty_yards: 5456,
+		rushing: {
+			total_rushing_yards: 46456,
+			total_rushing_touchdowns: 53
+		},
+		passing: {
+			total_passing_yards: 2233,
+			total_passing_touchdowns: 1234
+		},
+		special_teams: {
+			total_kick_return_yards: 42,
+			total_punt_return_yards: 23
+		},
+		turnovers: {
+			takeaways: {
+				interceptions: 745,
+				fumble_recoveries: 456
+			},
+			giveaways: {
+				interceptions: 78,
+				fumble_losses: 35
+			}
+        }
+    },
+}
